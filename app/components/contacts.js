0114f7d293d99b7b3ba8940f09f7773be0825ada
ahmad safiul ubab <safiul@syafi>
2017-04-16 01:28:21 +0700

    nampilno list contact

--- app/components/contacts.js
+++ app/components/contacts.js
@@ -51,22 +51,46 @@ export default class Contacts extends Component {
     super(props)
 
     this.state = {
-      dataSource: ds.cloneWithRows(data),
+      dataSource: ds.cloneWithRows([]),
     }
+
+    this.socket = this.props.socket;
   }
 
+  componentDidMount() {
+    this._onFetch();
+  }
+
+  componentWillMount() {
+    var self = this;
+    this.socket.on('listContact', function (hasil){
+      self.setState({
+        dataSource : ds.cloneWithRows(hasil)
+      });
+    });
+  }
+
+  _onFetch () {
+    AsyncStorage.getItem('session', (err, result) => {
+      if (result != null ) {
+        var obj = JSON.parse(result);
+        this.socket.emit('listContact',{_id : obj._id});
+      }
+    });
+  }
+
+  // <Text style={{ fontWeight:'400', color:'#666', fontSize:12, marginLeft:15 }}>{x.message}</Text>
   eachMessage(x){
     return (
       <TouchableOpacity>
         <View style={{ alignItems:'center', padding:10, flexDirection:'row', borderBottomWidth:1, borderColor:'#f7f7f7' }}>
-          <Image source={x.image} style={{ borderRadius:30, width:60, height:60 }} resizeMode='contain' />
+          <Image source={require('../images/eminem.jpg')} style={{ borderRadius:30, width:60, height:60 }} resizeMode='contain' />
           <View>
             <View style={{ flexDirection:'row', justifyContent:'space-between', width:280 }}>
-              <Text style={{ marginLeft:15, fontWeight:'600', color:'#222', fontSize:15 }}>{x.first_name}</Text>
+              <Text style={{ marginLeft:15, fontWeight:'600', color:'#222', fontSize:15 }}>{x.first_name} {x.last_name}</Text>
               <Text style={{ fontWeight:'200', color:'#777', fontSize:13 }}>MOBILE</Text>
             </View>
             <View style={{ flexDirection:'row', alignItems:'center' }}>
-              <Text style={{ fontWeight:'400', color:'#666', fontSize:12, marginLeft:15 }}>{x.message}</Text>
             </View>
           </View>
         </View>
@@ -78,6 +102,7 @@ export default class Contacts extends Component {
     return (
       <View style={{ flex:1 }}>
         <ListView
+          enableEmptySections={true}
           dataSource={this.state.dataSource}
           renderRow={(rowData) => this.eachMessage(rowData)}
         />
